02568e12f8e5fc519fa25e09bae0fee58e9962d1
Ukolabs40 <moh.tetuko@gmail.com>
2026-09-26 01:35:22 +0700

    fix(vercel): configure framework nextjs and backward compatible rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/cinematic', destination: '/' },
+      { source: '/sunblix_cinematic.html', destination: '/' },
+    ];
+  },
 };
 
 export default nextConfig;
